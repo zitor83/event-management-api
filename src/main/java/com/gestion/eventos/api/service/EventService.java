0f16dbd0bb34f5e9403c21a41460ef2cb1ed0364
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-20 23:09:44 +0100

    feat(event): implement complex relational mapping in EventService save method

--- src/main/java/com/gestion/eventos/api/service/EventService.java
+++ src/main/java/com/gestion/eventos/api/service/EventService.java
@@ -1,19 +1,29 @@
 package com.gestion.eventos.api.service;
 
+import com.gestion.eventos.api.domain.Category;
 import com.gestion.eventos.api.domain.Event;
+import com.gestion.eventos.api.domain.Speaker;
+import com.gestion.eventos.api.dto.EventRequestDto;
 import com.gestion.eventos.api.exception.ResourceNotFoundException;
+import com.gestion.eventos.api.mapper.EventMapper;
 import com.gestion.eventos.api.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class EventService implements IEventService{
 
     private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
+    private final CategoryService categoryService;
+    private final SpeakerService speakerService;
+
 
 
     @Override
@@ -23,8 +33,22 @@ public class EventService implements IEventService{
     }
 
     @Override
-    @Transactional()
-    public Event save(Event event) {
+    @Transactional
+    public Event save(EventRequestDto requestDto) {
+        Event event = eventMapper.toEntity(requestDto);
+
+        Category category = categoryService.findById(requestDto.getCategoryId());
+        event.setCategory(category);
+
+        if(requestDto.getSpeakerIds() != null && !requestDto.getSpeakerIds().isEmpty()){
+            Set<Speaker> speakers = requestDto.getSpeakerIds().stream()
+                    .map(speakerService::findById)
+                    .collect(Collectors.toSet());
+
+            speakers.forEach(event::addSpeaker);
+        }
+
+
         return eventRepository.save(event);
     }
 
@@ -34,6 +58,22 @@ public class EventService implements IEventService{
         return eventRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Evento no encontrado con id: " + id)
         );
+    }
+
+    @Override
+    public Event update(Long id, EventRequestDto requestDto) {
+        Event existingEvent= eventRepository.findById(id)
+                .orElseThrow(
+                    () -> new ResourceNotFoundException("Evento no encontrado con id: " + id)
+        );
+        eventMapper.updateEventFromDto(requestDto, existingEvent);
+
+        if(!existingEvent.getCategory().getId().equals(requestDto.getCategoryId())){
+            Category category = categoryService.findById(requestDto.getCategoryId());
+            existingEvent.setCategory(category);
+        }
+
+        return null;
     }
 
     @Override
--- src/main/java/com/gestion/eventos/api/service/IEventService.java
+++ src/main/java/com/gestion/eventos/api/service/IEventService.java
@@ -1,13 +1,15 @@
 package com.gestion.eventos.api.service;
 
 import com.gestion.eventos.api.domain.Event;
+import com.gestion.eventos.api.dto.EventRequestDto;
 
 import java.util.List;
 
 public interface IEventService {
     List<Event> findAll();
-    Event save(Event event);
+    Event save(EventRequestDto event);
     Event findById(Long id);
+    Event update(Long id, EventRequestDto requestDto);
     void deleteById(Long id);
 }
 
